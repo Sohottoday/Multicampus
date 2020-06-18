017835e5b88f23d172ac49a0e243c963913c3f4b
Sohottoday <human9062@naver.com>
2020-06-18 14:06:34 +0900

    firstlab

---
+++ Python/Django/django_lab/Lab1/movies/forms.py
@@ -0,0 +1,66 @@
+from django import forms
+from .models import Movie
+
+class MovieForm(forms.ModelForm):
+    title = forms.CharField(
+        label='',
+        widget=forms.TextInput(
+            attrs = {
+                'placeholder' : '영화명'
+            }
+        )
+    )
+    title_en = forms.CharField(
+        label='영화명(영문)',
+        widget=forms.TextInput(
+            attrs= {
+                'placeholder' : '영화명(영문)'
+            }
+        )
+    )
+    audience = forms.IntegerField(
+        label = '누적 관객수'
+    )
+    open_date = forms.DateInput(
+        
+    )
+    genre = forms.CharField(
+        label='장르',
+        widget=forms.TextInput(
+            attrs= {
+                'placeholder' : '장르'
+            }
+        )
+    )
+    watch_grade = forms.CharField(
+        label='관람등급',
+        widget=forms.TextInput(
+            attrs={
+                'placeholder' : '관람등급'
+            }
+        )
+    )
+    score = forms.FloatField(
+        label='평점'
+        
+    )
+    poster_url = forms.CharField(
+        label='포스터 이미지 URL',
+        widget=forms.Textarea(
+            attrs={
+                'placeholder' : '포스터 이미지 URL'
+            }
+        )
+    )
+    description = forms.CharField(
+        label='영화 소개',
+        widget=forms.Textarea(
+            attrs={
+                'placeholder' : '영화 소개'
+            }
+        )
+    )
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
---
+++ Python/Django/django_lab/Lab1/movies/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=40) # 영화명
+    title_en = models.CharField(max_length=40)  # 영화명(영문)
+    audience = models.IntegerField()    # 누적 관객수
+    open_date = models.DateField()  # 개봉일
+    genre = models.CharField(max_length=20) # 장르
+    watch_grade = models.CharField(max_length=20)   # 관람등급
+    score = models.FloatField() # 평점
+    poster_url = models.TextField() # 포스터 이미지 URL
+    description = models.TextField()    # 영화 소개
